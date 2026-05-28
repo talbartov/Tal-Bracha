3ac36cdce96400a0f88d9d4e446527d1354a68c5
talbartov <talba@eca.gov.il>
2026-05-28 15:12:26 +0300

    Update app.py

--- app.py
+++ app.py
@@ -1,3 +1,15 @@
-print("Hello Render")
+import streamlit as st
+import yfinance as yf
 
+st.title("זירת מסחר")
 
+symbol = st.text_input("הכנס סימול מניה", "AAPL")
+
+if st.button("בדיקה"):
+    stock = yf.Ticker(symbol)
+    data = stock.history(period="1d")
+
+    if not data.empty:
+        st.write("מחיר אחרון:", data["Close"].iloc[-1])
+    else:
+        st.write("אין נתונים")
